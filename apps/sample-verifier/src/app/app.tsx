a7ab226e20e321a47fd21537689d2f365e1b10a9
Yuren Ju <yurenju@gmail.com>
2023-08-24 11:10:20 +0800

    feat: implemented basic verifier

--- apps/sample-verifier/src/app/app.tsx
+++ apps/sample-verifier/src/app/app.tsx
@@ -1,9 +1,37 @@
-import NxWelcome from './nx-welcome';
+import { ChangeEvent } from 'react';
 
 export function App() {
+  const handleSelectOnDid = () => {
+    console.log('Select on DID');
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) {
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function (event: ProgressEvent<FileReader>) {
+      if (event.target?.result) {
+        console.log(event.target.result as string);
+      }
+    };
+    reader.readAsText(file);
+  };
+
   return (
     <div>
-      <NxWelcome title="sample-verifier" />
+      <button data-testid="select-on-did" onClick={handleSelectOnDid}>
+        Select on DID
+      </button>
+      <br />
+      <label htmlFor="credential-file">Select credential file: </label>
+      <input
+        type="file"
+        data-testid="credential-file"
+        onChange={handleFileChange}
+      />
     </div>
   );
 }
