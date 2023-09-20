18f54027e07f08515853efd19f896eb3acf9dbe1
Yuren Ju <yurenju@gmail.com>
2023-09-20 14:20:27 +0800

    refactor: extract string to constant

--- apps/server/src/ethereum/ethereum.strategy.ts
+++ apps/server/src/ethereum/ethereum.strategy.ts
@@ -3,13 +3,14 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-ethereum-siwe';
 import { NonceService } from './nonce.service';
 
+export const ETHEREUM_STRATEGY_NAME = 'ethereum';
 @Injectable()
 export class EthereumStrategy extends PassportStrategy(Strategy) {
   name: string;
 
   constructor(private nonceService: NonceService) {
     super({ store: nonceService });
-    this.name = 'ethereum';
+    this.name = ETHEREUM_STRATEGY_NAME;
   }
 
   async validate(address: string): Promise<{ address: string }> {
@@ -20,5 +21,3 @@ export class EthereumStrategy extends PassportStrategy(Strategy) {
     return this.nonceService.challenge();
   }
 }
-
-export const ETHEREUM_STRATEGY_NAME = 'ethereum';
