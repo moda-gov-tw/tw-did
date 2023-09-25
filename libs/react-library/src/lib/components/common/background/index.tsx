aaee7dd1874722cfc86c2c9bfa27deaa7a94b3a8
Foo Jia Wen <38941680+fjwntut@users.noreply.github.com>
2023-09-25 10:52:56 +0800

    fix bg disappear and layout on big screen

--- libs/react-library/src/lib/components/common/background/index.tsx
+++ libs/react-library/src/lib/components/common/background/index.tsx
@@ -1,7 +1,14 @@
 import styles from './background.module.scss';
 import { ReactNode } from 'react';
+import sm from './img/bg-sm.svg';
+import md from './img/bg-md.svg';
 
 export const BackgroundContainer = ({ children }: { children: ReactNode }) =>
     <div className={styles.Background} >
         {children}
     </div>;
+
+export const bgimgs = () => ({
+    sm,
+    md
+});
