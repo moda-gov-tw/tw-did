97e108302a4aa6bc6aa2ef620143bfa74baf39aa
Foo Jia Wen <38941680+fjwntut@users.noreply.github.com>
2023-09-25 21:07:31 +0800

    add Header

--- libs/react-library/src/lib/components/index.tsx
+++ libs/react-library/src/lib/components/index.tsx
@@ -1,4 +1,5 @@
-export { WelcomeScreen } from "./layout/actionScreen/welcomeScreen"
-export { RegisterScreen } from "./layout/actionScreen/registerScreen"
-export { LoginScreen } from "./layout/actionScreen/loginScreen"
-export { CredentialScreen } from "./layout/credentialScreen/credentialScreen"
+export { WelcomeScreen } from './layout/actionScreen/welcomeScreen';
+export { RegisterScreen } from './layout/actionScreen/registerScreen';
+export { LoginScreen } from './layout/actionScreen/loginScreen';
+export { CredentialScreen } from './layout/credentialScreen/credentialScreen';
+export { Header } from './layout/header/header';
---
+++ libs/react-library/src/lib/components/layout/header/header.tsx
@@ -0,0 +1,34 @@
+import styles from './header.module.scss';
+import theme from '../../../styles/theme.module.scss';
+import { GoIcon } from '../../common/icons/go';
+import { Button } from '../../common/button';
+
+interface StringByString {
+  [key: string]: string;
+}
+const pathToTitle: StringByString = {
+  '/register': 'Register',
+  '/login': 'Login',
+  '/select-credential': 'My Credentials',
+  '/ethereum-login': 'Ethereum Login',
+  '/semaphore': 'Semaphore',
+};
+
+export const Header = ({
+  path,
+  onBack,
+}: {
+  path: string;
+  onBack?: () => void;
+}) => {
+  return (
+    <>
+      {pathToTitle[path] && (
+        <div className={styles.header + ' ' + theme.theme_light}>
+            {onBack && <Button icon={GoIcon} onClick={onBack}/>}
+          <h1>{pathToTitle[path]}</h1>
+        </div>
+      )}
+    </>
+  );
+};
