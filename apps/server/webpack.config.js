88a5268aa5a7d2d5c96a48597d160fc28130ffb6
Yuren Ju <yurenju@gmail.com>
2023-10-18 09:19:58 +0800

    fix: fix esm compatibility issue

--- apps/server/webpack.config.js
+++ apps/server/webpack.config.js
@@ -1,8 +1,47 @@
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 const { composePlugins, withNx } = require('@nx/webpack');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
-  return config;
+  return {
+    ...config,
+    externals: nodeExternals({
+      modulesDir: path.join(__dirname, '../..', 'node_modules'),
+      allowlist: /^@veramo/,
+    }),
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.js$/,
+          include: /@veramo/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    modules: 'commonjs',
+                  },
+                ],
+              ],
+              plugins: [
+                [
+                  '@babel/plugin-syntax-import-attributes',
+                  {
+                    deprecatedAssertSyntax: true,
+                  },
+                ],
+              ],
+            },
+          },
+        },
+      ],
+    },
+  };
 });
