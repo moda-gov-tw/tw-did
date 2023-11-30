ee09c3a88c75b62ac12d0eb7dc78a5644fa9e64e
Yuren Ju <yurenju@gmail.com>
2023-11-30 11:47:12 +0800

    feat: added all filter

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,13 +3,24 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { LogLevel, Logger } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { AllExceptionsFilter } from './exception/all-exceptions.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logLevel: LogLevel[] = process.env.LOGGER_LEVEL
+    ? (process.env.LOGGER_LEVEL.split(',') as LogLevel[])
+    : ['log', 'error', 'warn', 'debug', 'verbose'];
+
+  const app = await NestFactory.create(AppModule, {
+    logger: logLevel,
+  });
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   const globalPrefix = 'api';
   app.enableCors();
   app.setGlobalPrefix(globalPrefix);
